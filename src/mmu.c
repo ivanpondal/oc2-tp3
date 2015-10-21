4c6d7c0f190b8bd2020fe8e6d0a285e5a1d581b4
Iván Pondal <ivan.pondal@gmail.com>
2015-10-21 14:12:50 -0300

    Refactor paging map/unmap routines

--- src/mmu.c
+++ src/mmu.c
@@ -29,8 +29,7 @@ uint mmu_inicializar_dir_kernel(){
 	*ptr_pde = *ptr_pde | BASE_DIR_PAGINAS_0;
 
 	uint direccion_pagina;
-	uint i;
-	for(i = 0; i < 1024; i++){
+	for(uint i = 0; i < 1024; i++){
 		direccion_pagina = i << 12;
 		mmu_mapear_pagina(direccion_pagina, BASE_DIR_TABLAS, direccion_pagina, 0x3);
 	}
@@ -39,29 +38,29 @@ uint mmu_inicializar_dir_kernel(){
 }
 
 void mmu_mapear_pagina(uint virtual, uint cr3, uint fisica, uint attrs){
-	uint base_directorio = 0xFFFFF000 & cr3;
-	uint offset_directorio_virtual = 4*((0xFFC00000 & virtual) >> 22);
-	uint* ptr_pde_virtual = (uint*)(base_directorio + offset_directorio_virtual);
-	uint pde_virtual = *ptr_pde_virtual;
+	uint base_directorio_tablas = 0xFFFFF000 & cr3;
+	uint offset_directorio_tablas = 4*((0xFFC00000 & virtual) >> 22);
+	uint* ptr_pde = (uint*)(base_directorio_tablas + offset_directorio_tablas);
+	uint pde = *ptr_pde;
 
-	uint base_directorio_tabla = 0xFFFFF000 & pde_virtual;
-	uint offset_directorio_tabla = 4*((0x003FF000 & virtual) >> 12);
-	uint* ptr_pte_virtual = (uint*)(base_directorio_tabla + offset_directorio_tabla);
+	uint base_directorio_paginas = 0xFFFFF000 & pde;
+	uint offset_directorio_paginas = 4*((0x003FF000 & virtual) >> 12);
+	uint* ptr_pte = (uint*)(base_directorio_paginas + offset_directorio_paginas);
 
-	*ptr_pte_virtual = (0x00000FFF & virtual) | (0xFFFFF000 & fisica) | attrs;
+	*ptr_pte = (0xFFFFF000 & fisica) | attrs;
 }
 
 uint mmu_unmapear_pagina(uint virtual, uint cr3){
-	uint base_directorio = 0xFFFFF000 & cr3;
-	uint offset_directorio_virtual = 4*((0xFFC00000 & virtual) >> 22);
-	uint* ptr_pde_virtual = (uint*)(base_directorio + offset_directorio_virtual);
-	uint pde_virtual = *ptr_pde_virtual;
+	uint base_directorio_tablas = 0xFFFFF000 & cr3;
+	uint offset_directorio_tablas = 4*((0xFFC00000 & virtual) >> 22);
+	uint* ptr_pde = (uint*)(base_directorio_tablas + offset_directorio_tablas);
+	uint pde = *ptr_pde;
 
-	uint base_directorio_tabla = 0xFFFFF000 & pde_virtual;
-	uint offset_directorio_tabla = 4*((0x003FF000 & virtual) >> 12);
-	uint* ptr_pte_virtual = (uint*)(base_directorio_tabla + offset_directorio_tabla);
+	uint base_directorio_paginas = 0xFFFFF000 & pde;
+	uint offset_directorio_paginas = 4*((0x003FF000 & virtual) >> 12);
+	uint* ptr_pte = (uint*)(base_directorio_paginas + offset_directorio_paginas);
 
-	*ptr_pte_virtual = *ptr_pte_virtual & 0xFFFFFFFE;
+	*ptr_pte = *ptr_pte & 0xFFFFFFFE;
 
 	return 0;
 }
