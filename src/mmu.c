c8e4b3546b7a9692409494052843ab887b701448
Iván Pondal <ivan.pondal@gmail.com>
2015-10-25 16:59:33 -0300

    Add present bit chech for address unmapping

--- src/mmu.c
+++ src/mmu.c
@@ -30,8 +30,7 @@ uint mmu_proxima_pagina_fisica_libre(){
 }
 
 void mmu_inicializar_pagina(uint * pagina){
-	uint i;
-	for(i = 0; i < 4096; i++){
+	for(uint i = 0; i < 4096; i++){
 		pagina[i] = 0;
 	}
 }
@@ -49,8 +48,7 @@ uint mmu_inicializar_dir_kernel(){
 	*ptr_pde = *ptr_pde | BASE_DIR_PAGINAS_0;
 
 	uint direccion_pagina;
-	uint i;
-	for(i = 0; i < 1024; i++){
+	for(uint i = 0; i < 1024; i++){
 		direccion_pagina = i << 12;
 		mmu_mapear_pagina(direccion_pagina, BASE_DIR_TABLAS, direccion_pagina, 0x3);
 	}
@@ -64,19 +62,20 @@ void mmu_mapear_pagina(uint virtual, uint cr3, uint fisica, uint attrs){
 	uint* ptr_pde = (uint*)(base_directorio_tablas + offset_directorio_tablas);
 	uint pde = *ptr_pde;
 
-	uint base_directorio_paginas = 0xFFFFF000 & pde;
+	// Si no esta presente la tabla de páginas que buscamos
 	if ((pde & 0x1) == 0){
 		uint pt = mmu_proxima_pagina_fisica_libre();
 		mmu_inicializar_pagina(&pt);
-		*ptr_pde = pt;
+		*ptr_pde = pt;	// Actualizamos el pde con la nueva dirección de la tabla de páginas
 	}
 
-	*ptr_pde = 0x3 | *ptr_pde;  //Seteamos el present y el write en el pde, 
-								//para poder modificar el pte
+	*ptr_pde = 0x3 | *ptr_pde;	// Seteamos el present y el write en el pde, para poder modificar el pte
+	uint base_directorio_paginas = 0xFFFFF000 & pde;
+
 	uint offset_directorio_paginas = 4*((0x003FF000 & virtual) >> 12);
 	uint* ptr_pte = (uint*)(base_directorio_paginas + offset_directorio_paginas);
 	*ptr_pte = (0xFFFFF000 & fisica) | attrs;
-	tlbflush();					//Vaciamos la TLB 
+	tlbflush();	// Vaciamos la TLB para que se reflejen los cambios
 }
 
 uint mmu_unmapear_pagina(uint virtual, uint cr3){
@@ -85,12 +84,16 @@ uint mmu_unmapear_pagina(uint virtual, uint cr3){
 	uint* ptr_pde = (uint*)(base_directorio_tablas + offset_directorio_tablas);
 	uint pde = *ptr_pde;
 
-	uint base_directorio_paginas = 0xFFFFF000 & pde;
-	uint offset_directorio_paginas = 4*((0x003FF000 & virtual) >> 12);
-	uint* ptr_pte = (uint*)(base_directorio_paginas + offset_directorio_paginas);
+	// Si está presente la tabla de páginas asociada a la dirección procedemos a
+	// deshabilitar la página correspondiente
+	if ((pde & 0x1) == 1){
+		uint base_directorio_paginas = 0xFFFFF000 & pde;
+		uint offset_directorio_paginas = 4*((0x003FF000 & virtual) >> 12);
+		uint* ptr_pte = (uint*)(base_directorio_paginas + offset_directorio_paginas);
 
-	*ptr_pte = *ptr_pte & 0xFFFFFFFE;
-	tlbflush();
+		*ptr_pte = *ptr_pte & 0xFFFFFFFE;
+		tlbflush();
+	}
 
 	return 0;
 }
