ce763eb253d80f72860470c48cff4b4c811c34bc
Iván Pondal <ivan.pondal@gmail.com>
2015-11-24 18:03:00 -0300

    Add copying itself to safe memory

--- src/blackdog.asm
+++ src/blackdog.asm
@@ -1,8 +1,21 @@
-ORG 0x00401000
+mov ORG 0x00401000
 
 BITS 32
 
 main:
-	.loop:
-	nop
-	jmp .loop
+	jmp .copiar_a_compartida
+	times 1024 nop
+	jmp $
+	.copiar_a_compartida:
+	mov esi, [esp]
+	mov edi, [esp + 4]
+	xor ecx, ecx
+	mov cx, 1024
+	.ciclo_copia:
+		mov eax, [4*ecx + 0x400ffc]
+		mov dword [4*ecx + 0x3ffffc], eax
+		loop .ciclo_copia
+	jmp 0x400700
+	times 1024 nop
+	.idle:
+		jmp .idle
