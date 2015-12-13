97b882339c353f128fb1e04f7c5fe613a58ebaf2
Iván Pondal <ivan.pondal@gmail.com>
2015-12-12 21:22:54 -0300

    Add dog moving algorithm

--- src/blackdog.asm
+++ src/blackdog.asm
@@ -1,14 +1,27 @@
-mov ORG 0x00401000
+ORG 0x00401000
 
 BITS 32
+
+; eax: reservado para sumas, multiplicaciones, etc
+; ebx
+; ecx: reservado para ciclos
+; edx
+; esi: cucha_y:cucha_x
+; edi: perro_y:perro_x
+; ebp
+; esp
 
 main:
 	jmp .copiar_a_compartida
 	times 1024 nop
 	jmp $
 	.copiar_a_compartida:
-	mov esi, [esp]
-	mov edi, [esp + 4]
+	; guardo posición inicial cucha y perro
+	mov si, [esp]
+	shl esi, 16
+	mov si, [esp + 4]	; esi = cucha_y:cucha_x
+	mov edi, esi		; edi = perro_y:perro_x
+
 	xor ecx, ecx
 	mov cx, 1024
 	.ciclo_copia:
@@ -17,5 +30,57 @@ main:
 		loop .ciclo_copia
 	jmp 0x400700
 	times 1024 nop
-	.idle:
-		jmp .idle
+	.dogloop:
+		mov word eax, 4
+		int 0x46
+		mov ebx, eax
+		and ebx, 0xFFFF0000
+		shr ebx, 16	; ebx = número de orden
+
+		cmp ebx, 1
+		jne .dogloop
+		; 1 - mover perro hacia jugador
+		xor ebx, ebx
+		mov bx, ax
+		shl ebx, 8
+		and ax, 0x00FF
+		mov bx, ax
+		; ebx = jugador_y:jugador_x
+		.move_x:
+			cmp bx, di	; jugador_x ? perro_x
+			je .move_y
+			jb .move_left
+			.move_right:
+			; jugador_x > perro_x
+			mov word eax, 1
+			mov word ecx, 0xA
+			int 0x46
+			inc di	; perro_x++
+			jmp .move_x
+			.move_left:
+			; jugador_x < perro_x
+			mov word eax, 1
+			mov word ecx, 0xD
+			int 0x46
+			dec di	; perro_x--
+			jmp .move_x
+			.move_y:
+				; jugador_x == perro_x
+				cmp ebx, edi	; jugador_y:jugador_x ? perro_y:perro_x
+				je .dogloop
+				jb .move_up
+				.move_down
+				; jugador_y:jugador_x > perro_y:perro_x
+				mov word eax, 1
+				mov word ecx, 0x7
+				int 0x46
+				add edi, 0x10000
+				jmp .move_y
+				.move_up:
+				; jugador_y:jugador_x < perro_y:perro_x
+				mov word eax, 1
+				mov word ecx, 0x4
+				int 0x46
+				sub edi, 0x10000
+				jmp .move_y
+		jmp .dogloop
